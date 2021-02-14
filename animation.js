6e7105c5480639e5d311026f4ca3963c4d812975
takeyy <t0k6yist@gmail.com>
2021-02-14 23:50:38 +0900

    大幅に改善

--- animation.js
+++ animation.js
@@ -1,13 +1,22 @@
 const header = document.getElementById("header");
 let degree = 0;
 function rotateHeader() {
-    degree = degree + 3;
-    degree = degree % 360;
-    if (0 <= degree && degree <= 90 || 270 <= degree && degree <= 360) {
-        header.className = "face";
-    } else {
-        header.className = "back";
-    }
-    header.style.transform = "rotateX(" + degree + "deg)";
+  degree = degree + 3;
+  degree = degree % 360;
+  if (0 <= degree && degree <= 90 || 270 <= degree && degree <= 360) {
+    header.className = "face";
+  } else {
+    header.className = "back";
+  }
+  header.style.transform = "rotateX(" + degree + "deg)";
 }
 setInterval(rotateHeader, 20);
+
+$(function () {
+var offset = 53;
+  $('.navbar li a').click(function (event) {
+    event.preventDefault();
+    location.replace($(this).attr('href'));
+    window.scrollBy(0, -offset);
+  });
+});
